ac9b4ec4369af27507cf74961f33d46dac58f025
Svetlana <xodik@mail.ru>
2018-03-03 12:39:10 +0300

    update

--- desktop/src/ru/simagin/game/desktop/DesktopLauncher.java
+++ desktop/src/ru/simagin/game/desktop/DesktopLauncher.java
@@ -7,7 +7,7 @@ import ru.simagin.game.SlotMachineGame;
 public class DesktopLauncher {
     public static void main(String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.title = "VaultSlot";
+        //config.title = "VaultSlot";
         config.width = 800;
         config.height = 480;
         config.resizable = false;
